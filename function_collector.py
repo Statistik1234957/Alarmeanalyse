3f65fc34017439e9abc6b915090dd0d8b05bec23
Statistik1234957 <73113906+Statistik1234957@users.noreply.github.com>
2021-02-25 14:48:23 +0100

    Add files via upload

--- function_collector.py
+++ function_collector.py
@@ -1,42 +1,6 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
-import ssl
-import logging
-import sys
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-def ssl_alpn(ca_cert, certfile, certkey):
-
-    "This function deals with the certificates"
-
-    try:
-        ssl_context = ssl.create_default_context()
-        ssl_context.load_verify_locations(cafile=ca_cert)
-        ssl_context.load_cert_chain(certfile=certfile, keyfile=certkey)
-
-        return ssl_context
-
-    except Exception as e:
-        print("exception ssl_alpn()")
-        raise e
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-def createlogger():
-
-    "This function creates a logger"
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler(sys.stdout)
-    log_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    handler.setFormatter(log_format)
-    logger.addHandler(handler)
-
-    return logger
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 def api_time_correction(df):
 
